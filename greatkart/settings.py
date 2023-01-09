ff41b827ff889f97ddaf6e881809ba2fbc493c94
Srikant <srikantddazzler387@gmail.com>
2023-01-09 18:27:43 +0530

    new file:   carts/__init__.py 	new file:   carts/admin.py 	new file:   carts/apps.py 	new file:   carts/context_processor.py 	new file:   carts/migrations/0001_initial.py 	new file:   carts/migrations/__init__.py 	new file:   carts/models.py 	new file:   carts/tests.py 	new file:   carts/urls.py 	new file:   carts/views.py 	modified:   greatkart/settings.py 	modified:   greatkart/urls.py 	modified:   manage.py 	modified:   store/admin.py 	new file:   store/migrations/0002_variation.py 	modified:   store/models.py 	modified:   store/urls.py 	modified:   store/views.py 	modified:   templates/includes/navbar.html 	new file:   templates/store/cart.html 	modified:   templates/store/product_detail.html 	modified:   templates/store/store.html

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    
+    'carts',
+
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processor.counter',
             ],
         },
     },
@@ -75,7 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'greatkart.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.Account'
-
 
 
 # Database
@@ -126,12 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-        'greatkart/static',
+    'greatkart/static',
 ]
 
 # media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
-
+MEDIA_ROOT = BASE_DIR / 'media'
