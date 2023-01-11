5d22a5b5939964f53036afb8988678dbbdd640a2
Srikant <srikantddazzler387@gmail.com>
2023-01-11 20:42:37 +0530

    Third Commit

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 
 # Create your models here.
+
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
@@ -10,15 +11,16 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
 
+
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    variations = models.ManyToManyField(Variation, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
     def sub_total(self):
         return self.product.price * self.quantity
 
-
-    def __str__(self):
+    def __unicode__(self):
         return self.product
